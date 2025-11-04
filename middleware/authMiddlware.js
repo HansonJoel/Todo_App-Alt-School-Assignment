20d8971f9d118643b18b99279d8240c22448f4cd
HansonJoel <joelhanson002@gmail.com>
2025-11-04 17:24:51 +0100

    Add files via upload
    added a new middleware directory

---
+++ middleware/authMiddlware.js
@@ -0,0 +1,98 @@
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+const UserModel = require("../models/user");
+
+const JWTstrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+
+passport.use(
+  new JWTstrategy(
+    {
+      secretOrKey: process.env.JWT_SECRET,
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(), // Use this if you are using Bearer token
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+
+// This middleware saves the information provided by the user to the database,
+// and then sends the user information to the next middleware if successful.
+// Otherwise, it reports an error.
+passport.use(
+  "signup",
+  new localStrategy(
+    {
+      usernameField: "username", //field used for login
+      passwordField: "password", // field used for login
+      passReqToCallback: true, // gives acces to req.body
+    },
+    async (req, username, password, done) => {
+      try {
+        // Extract fields from form
+        const { firstName, lastName, email, phone, gender, day, month, year } =
+          req.body;
+
+        // ✅ Group date fields correctly
+        const dateOfBirth = {
+          day,
+          month,
+          year,
+        };
+
+        // create the user with all fields
+        const user = await UserModel.create({
+          firstName,
+          lastName,
+          username,
+          email,
+          password,
+          phone,
+          gender,
+          dateOfBirth,
+        });
+
+        return done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+
+// This middleware authenticates the user based on the email and password provided.
+// If the user is found, it sends the user information to the next middleware.
+// Otherwise, it reports an error.
+passport.use(
+  "login",
+  new localStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+    },
+    async (username, password, done) => {
+      try {
+        const user = await UserModel.findOne({ username });
+
+        if (!user) {
+          return done(null, false, { message: "Username not found" });
+        }
+
+        const validate = await user.isValidPassword(password);
+
+        if (!validate) {
+          return done(null, false, { message: "Wrong Password" });
+        }
+
+        return done(null, user, { message: "Logged in Successfully" });
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
